1eab12ee0907d7748c537b4704500b45619034a8
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-19 17:52:37 +0400

    Alpine

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -2,7 +2,20 @@
     <h2>
         New Post:
     </h2>
-    <form wire:submit="save">
+    <div x-data="{count: 0}">
+        <span x-text="count"></span>
+        <button x-on:click="count++">
+            +
+        </button>
+    </div>
+    Current Title: <span x-text="$wire.title"></span>
+    <button x-on:click="$wire.title = ''">
+        Clear Title
+    </button>
+    <button type="button" x-on:click="$wire.save()">
+        Submit Form
+    </button>
+    <form wire:submit="save" {{--x-on:submit.prevent="$wire.save()"--}}>
         <label>
             <span>
                 Title
@@ -19,6 +32,12 @@
                 Content
             </span>
             <textarea wire:model="content"></textarea>
+            <small>
+                Characters: <span x-text="$wire.content.length"></span>
+            </small>
+            <small>
+                Words: <span x-text="$wire.content.split(' ').length"></span>
+            </small>
             @error('content')
             <em>
                 {{ $message }}
