46b550af413c9733b25a064615e7fffdd3297c26
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-21 14:59:38 +0400

    Testing

---
+++ tests/Feature/Livewire/CreatePostTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use App\Livewire\CreatePost;
+use App\Models\Post;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class CreatePostTest extends TestCase {
+    /** @test */
+    public function renders_successfully() {
+        Livewire::test(CreatePost::class)
+            ->assertStatus(200);
+    }
+
+    public function can_create_new_post() {
+        $post = Post::whereTitle('Some title')->first();
+        $this->assertNull($post);
+
+        Livewire::test(Create::class)->setTitle('Some title')->setContent('Some content')->call('save');
+
+        $post = Post::whereTitle('Some title')->first();
+        $this->assertNotNull($post);
+        $this->assertEquals('Some title', $this->title);
+        $this->assertEquals('Some content', $this->content);
+    }
+    public function title_is_required() {
+        Livewire::test(Create::class)->set('title', '')->call('save')->assertHasErrors(['title' => 'min: 4']);
+    }
+}
